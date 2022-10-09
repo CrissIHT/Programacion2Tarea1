bb7db4b276051cb414a3e97854a24d5f98679562
MatiasMendozaa <mmendoza2019@udec.cl>
2022-10-09 16:23:25 -0300

    actualizacion de Articulo

--- Tarea1P2/src/tarea1p2/Articulos.java
+++ Tarea1P2/src/tarea1p2/Articulos.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package tarea1p2;
+package tarea1progra2;
 
-/**
- *
- * @author petroman
- */
-public class Articulos {
-    
+public class Articulo {
+    private int cantidad;
+    private float peso;
+    private float precio;
+    private String nombre;
+    private String descripcion;
+    public Articulo(String nombre, String descripcion, float precio, float peso){
+        this.nombre=nombre;
+        this.descripcion=descripcion;
+        this.peso=peso;
+        this.precio=precio;
+    }
+    public String gettNombre(){
+        return nombre;
+    }
+    public String toString(){
+        return descripcion;
+    }
+    public float gettPeso(){
+        return peso;
+    }
+    public float gettPrecioSI(){
+        return precio*cantidad;
+    }
+    public float gettPrecioCI(){
+        return ((precio*19/100f)+(precio))*cantidad;
+    }
+    public float gettIva(){
+        return (precio*19/100f)*cantidad;
+    }
+    public void setCant(int cant){
+        this.cantidad=cant;
+    }
+    public int settCant(){
+        return cantidad;
+    }
 }
